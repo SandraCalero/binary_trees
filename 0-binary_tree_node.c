09d968d9395988dbecf5b04b69683b09a2ad6cc8
Sandra Calero <3358@holbertonschool.com>
2021-11-30 19:18:23 -0500

    add a function that checks if a node is a leaf

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_node - Creates a binary tree node
  * @parent: Pointer to the parent node of the node to create
@@ -6,6 +7,7 @@
  *
  * Return: A pointer to the new node, or NULL on failure
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_bt_node = malloc(sizeof(binary_tree_t));
