9ee4fb4603fae034800e23685484eff7ac9ce7eb
Sandra Calero <3358@holbertonschool.com>
2021-12-01 10:16:19 -0500

    add edge case func NULL and fix betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,13 +4,14 @@
  * binary_tree_postorder - Goes through a binary tree using post-order
  * traversal
  * @tree: Pointer to the root node of the tree to delete
+ * @func: Pointer to a function to call for each node.
  *
  * Return: Nothing
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
+	if (tree == NULL || func == NULL)
 		return;
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
