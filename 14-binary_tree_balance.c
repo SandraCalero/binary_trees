e6dd8a590a38c1d6be662650b1ab8a6b95400b18
Juanse1595 <3309@holbertonschool.com>
2021-12-01 23:43:49 +0000

    FIX betty 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - Measures the height of a binary tree
+ * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: Pointer to the root node of the tree to delete
  *
  * Return: Height of the tree
@@ -11,6 +11,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int left_side;
 	int right_side;
+
 	if (tree == NULL)
 		return (0);
 	left_side = calculate_height(tree->left);
