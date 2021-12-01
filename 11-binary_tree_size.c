6d8b41e76792329a11ea2a1b9884607ebf3e4c9c
Juanse1595 <3309@holbertonschool.com>
2021-12-01 18:04:57 +0000

    ADD 11th task file

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Calculates the size of a binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * Return: Nothing
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	int left_side;
+	int right_side;
+
+	if (tree == NULL)
+		return (0);
+	left_side = binary_tree_size(tree->left);
+	right_side = binary_tree_size(tree->right);
+	return (left_side + right_side + 1);
+}
